3ccdcf19be2e46add1cab5fef27becca9a9b81e5
ChulHee Kim <solroya@gamil.com>
2024-10-20 02:10:10 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1. 도메인 클래스 ==1 테이블로 매칭되도록 설계
    생성자와 equals(), hashcode()등이 도메인 맥락에 알맞게 설정함

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,15 +1,86 @@
 package com.fastcampus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class ArticleComment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    //다대일 관계(여러 개의 자식 엔티티가 하나의 부모 엔티티와 연결)
+    //하나의 게시글(Article)에는 여러개의 댓글(ArticleComment)가 있음
+    //ArticleComment 엔티티에는 article_id라는 외래키가 자동으로 생성됨
+    @ManyToOne(optional = false)
     private Article article;
+
+    @Setter
+    @Column(nullable = false, length = 500)
     private String content;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt; // 생성 일시
+
+    @CreatedBy
+    @Column(nullable = false)
+    private String createdBy; // 생성자
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
-    private String modifiedBy;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy; // 수정자
+
+
+    protected ArticleComment() {
+
+    }
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+
 }
